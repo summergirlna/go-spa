c1ca26fd46ebdc2348048548ce33fedb87cd9e85
kurita <summergirlna@gmail.com>
2025-04-23 16:07:38 +0900

    feにkeycloak組み込み

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,6 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-//
-// function App() {
-//   const [count, setCount] = useState(0)
-//
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-//
-// export default App
-
 import {useEffect, useState} from "react";
 import axios from "axios";
+import keycloak from "./keycloak.ts";
 
 type User = {
     id: number
@@ -43,13 +8,32 @@ type User = {
 }
 
 function App() {
+    const [authenticated, setAuthenticated] = useState(false)
     const [users, setUsers] = useState<User[]>([])
 
     useEffect(() => {
-        axios.get<User[]>('http://localhost:8080/users')
-            .then(res => setUsers(res.data))
-            .catch(err => console.error('API error: ', err))
+        keycloak.init({ onLoad: 'login-required' }).then(auth => {
+            setAuthenticated(auth)
+
+            if (auth) {
+                axios.get('/api/users', {
+                    headers: {
+                        Authorization: `Bearer ${keycloak.token}`,
+                    }
+                }).then(res => {
+                    setUsers(res.data)
+                }).catch(err => {
+                    console.error('API error:', err)
+                })
+            }
+
+        })
+
     }, [])
+
+    if (!authenticated) {
+        return <div>Loading...</div>
+    }
 
     return (
         <div>
@@ -59,6 +43,8 @@ function App() {
                     <li key={user.id}>{user.id}: {user.name}</li>
                 ))}
             </ul>
+
+            <button onClick={() => keycloak.logout()}>ログアウト</button>
         </div>
     )
 }
